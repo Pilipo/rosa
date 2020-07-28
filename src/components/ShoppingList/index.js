1c2ef060e7bf337d0788d808d6de37367978e863
Phillip <pl@philliplehner.com>
2020-07-28 10:31:49 -0500

    Starts shopping list component

---
+++ src/components/ShoppingList/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ShoppingList = (props) => (
+    <div>
+      <h1>Shopping List</h1>
+      <p>Build a list of selected recipes.</p>
+    </div>
+);
+
+export default ShoppingList;
