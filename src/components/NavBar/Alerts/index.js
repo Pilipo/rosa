b303715bc45d354bc89fe06595a03f3144bb796d
Phillip <pl@philliplehner.com>
2020-07-28 11:58:48 -0500

    Nav Bar Updated for styles. I'm procrastinating...

--- src/components/NavBar/Alerts/index.js
+++ src/components/NavBar/Alerts/index.js
@@ -3,47 +3,54 @@ import React from 'react';
 const Alerts = () => (
 <li className="nav-item dropdown no-arrow mx-1">
         <a className="nav-link dropdown-toggle" href="/#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i className="fas fa-bell fa-fw"></i>
-          <span className="badge badge-danger badge-counter">3+</span>
+          <i className="fas fa-clipboard-list fa-fw"></i>
+          <span className="badge badge-danger badge-counter">4</span>
         </a>
         <div className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
           <h6 className="dropdown-header">
-            Alerts Center
+            Your Menu
           </h6>
           <a className="dropdown-item d-flex align-items-center" href="/#">
             <div className="mr-3">
-              <div className="icon-circle bg-primary">
-                <i className="fas fa-file-alt text-white"></i>
+              <div className="icon-circle bg-success">
+                <i className="fas fa-coffee text-white"></i>
               </div>
             </div>
             <div>
-              <div className="small text-gray-500">December 12, 2019</div>
-              <span className="font-weight-bold">A new monthly report is ready to download!</span>
+              <div className="small text-gray-500">Breakfast</div>
+              <ul className="list-unstyled">
+                <li>Quiche</li>
+                <li>Biscuits</li>
+              </ul>
             </div>
           </a>
           <a className="dropdown-item d-flex align-items-center" href="/#">
             <div className="mr-3">
               <div className="icon-circle bg-success">
-                <i className="fas fa-donate text-white"></i>
+                <i className="fas fa-hamburger text-white"></i>
               </div>
             </div>
             <div>
-              <div className="small text-gray-500">December 7, 2019</div>
-              $290.29 has been deposited into your account!
+              <div className="small text-gray-500">Lunch</div>
+              <ul className="list-unstyled">
+                <li>PB and J</li>
+              </ul>
             </div>
           </a>
           <a className="dropdown-item d-flex align-items-center" href="/#">
             <div className="mr-3">
-              <div className="icon-circle bg-warning">
-                <i className="fas fa-exclamation-triangle text-white"></i>
+              <div className="icon-circle bg-success">
+                <i className="fas fa-utensils text-white"></i>
               </div>
             </div>
             <div>
-              <div className="small text-gray-500">December 2, 2019</div>
-              Spending Alert: We've noticed unusually high spending for your account.
+              <div className="small text-gray-500">Dinner</div>
+              <ul className="list-unstyled">
+                <li>Sushi Bowl</li>
+              </ul>
             </div>
           </a>
-          <a className="dropdown-item text-center small text-gray-500" href="/#">Show All Alerts</a>
+          <a className="dropdown-item text-center small text-gray-500" href="/#">Show the List</a>
         </div>
       </li>);
 
