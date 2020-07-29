96a3d830f40381326d7c06071b814587c8a53d8d
Phillip <pl@philliplehner.com>
2020-07-29 16:58:20 -0500

    focus shifting on Enter press

--- src/components/Recipe/CreateRecipe.js
+++ src/components/Recipe/CreateRecipe.js
@@ -1,57 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button } from '@material-ui/core';
 
-function CreateRecipe() {
-  return (
-    <div className="col-xl-4 col-lg-6 mb-4">
-      <div className="card border-left-success shadow h-100">
-        <div className="card-header py-3">
-          <div className="row no-gutters align-items-center">
-            <div className="col mr-2">
-              <h6 className="m-0 font-weight-bold text-primary">Create a Recipe</h6>
-            </div>
-            <div className="col-auto">
-              <i className="fas fa-folder-plus fa-2x text-gray-300"></i>
-            </div>
+function handleEnter(event) {
+  if (event.keyCode === 13) {
+    const { form } = event.target;
+    const index = Array.prototype.indexOf.call(form, event.target);
+    form.elements[index + 1].focus();
+    event.preventDefault();
+  }
+}
+
+const CreateRecipe = () => (
+  <div className="col-xl-4 col-lg-6 mb-4">
+    <div className="card border-left-success shadow h-100">
+      <div className="card-header py-3">
+        <div className="row no-gutters align-items-center">
+          <div className="col mr-2">
+            <h6 className="m-0 font-weight-bold text-primary">Create a Recipe</h6>
+          </div>
+          <div className="col-auto">
+            <i className="fas fa-folder-plus fa-2x text-gray-300"></i>
           </div>
         </div>
-        <div className="card-body">
-          <div className="row no-gutters align-items-center">
-            <div className="col mr-2">
-              <form className="user" >
+      </div>
+      <div className="card-body">
+        <div className="row no-gutters align-items-center">
+          <div className="col mr-2">
+            <form className="user" >
+            <TextField
+                label="Name"
+                fullWidth
+                margin="normal"
+                onKeyDown={handleEnter}
+              />
               <TextField
-                  label="Name"
-                  fullWidth
-                  margin="normal"
-                />
-                <TextField
-                  label="Yield"
-                  fullWidth
-                  margin="normal"
-                />
-                <hr />
-                <TextField
-                  label="Ingredient"
-                  fullWidth
-                  margin="normal"
-                  helperText="[enter] to add another..."
-                />
-                <TextField
-                  label="Method"
-                  fullWidth
-                  multiline
-                  margin="normal"
-                  helperText="[enter] to add another..."
-                />
-                <hr className="dropdown-divider" />
-                <Button variant="contained" color="primary">Save Recipe</Button>
-              </form>
-            </div>
+                label="Yield"
+                fullWidth
+                margin="normal"
+                onKeyDown={handleEnter}
+              />
+              <hr />
+              <TextField
+                label="Ingredient"
+                fullWidth
+                margin="normal"
+                helperText="[return] to save and add another..."
+                onKeyDown={handleEnter}
+              />
+              <TextField
+                label="Method"
+                fullWidth
+                multiline
+                margin="normal"
+                helperText="[return] to save and add another..."
+                onKeyDown={handleEnter}
+              />
+              <hr className="dropdown-divider" />
+              <Button variant="contained" color="primary">Save Recipe</Button>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  );
-}
+  </div>
+);
 
 export default CreateRecipe;
