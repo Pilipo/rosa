1287e3eec8814f335e68dfe3a435a5090a487f98
Phillip <pl@philliplehner.com>
2020-07-30 15:26:41 -0500

    securing Landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { withToast } from '../Toast';
+import { AuthUserContext } from '../Session';
 import CreateRecipe from '../Recipe/CreateRecipe';
 import MakeShoppingList from '../ShoppingList/MakeShoppingList';
 import CheckShoppingList from '../ShoppingList/CheckShoppingList';
@@ -8,9 +9,9 @@ import CheckShoppingList from '../ShoppingList/CheckShoppingList';
 const Landing = () => (
   <div className="container-fluid">
   <div className="row">
-    <CreateRecipe />
-    <MakeShoppingList />
-    <CheckShoppingList />
+  <AuthUserContext.Consumer>
+      {(authUser) => (authUser ? <><CreateRecipe /> <MakeShoppingList /> <CheckShoppingList /></> : <h1>Welcome</h1>)}
+    </AuthUserContext.Consumer>
   </div>
 </div>
 );
